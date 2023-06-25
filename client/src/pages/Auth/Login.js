12d908712fefaa1c164ff5a1bd06d04bf547a614
ankpal145 <97392986+ankpal145@users.noreply.github.com>
2023-06-25 15:04:28 +0530

    Update Login.js

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,31 +1,35 @@
 import React, { useState } from "react";
 import Layout from "./../../components/Layout/Layout";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import toast  from 'react-hot-toast';
+import { useNavigate, useLocation } from "react-router-dom";
+import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
-
+import { useAuth } from "../../context/auth";
 const Login = () => {
-        
-        const [email, setEmail] = useState("");
-        const [Password, setPassword] = useState("");
-        
-        
-        const navigate = useNavigate();
-        // form function
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // form function
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const res = await axios.post("/api/v1/auth/login", {
-      
         email,
         password,
-        
-        
       });
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
-        navigate("/");
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+        localStorage.setItem("auth", JSON.stringify(res.data));
+        navigate(location.state || "/");
       } else {
         toast.error(res.data.message);
       }
@@ -34,45 +38,54 @@ const Login = () => {
       toast.error("Something went wrong");
     }
   };
-  return (
-    <Layout title="Register --Ecommerce App">
-    <div className="form-container">
-      <form onSubmit = {handleSubmit}>
-        <h4 className = "title">LoGIN FORM</h4>
-      
-        <div className="mb-3">
-          
-        </div>
-        <div className="mb-3">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="form-control"
-            id="exampleInputEmail1"
-            placeholder="Enter Your Email "
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="password"
-            value={Password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Enter Your Password"
-            required
-          />
-        </div>
-       
-        <button type="submit" className="btn btn-primary">
-          LOGIN
-        </button>
-      </form>
-    </div>
-  </Layout>
-  )
-}
 
-export default Login
+  return (
+    <Layout title="Register - Ecommerce App">
+      <div className="form-container" style={{ minHeight: "90vh" }}>
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">LOGIN FORM</h4>
+          <div className="mb-3"></div>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email "
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={() => {
+                navigate("/forgot-password");
+              }}
+            >
+              Forgot Password
+            </button>
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            LOGIN
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+export default Login;
