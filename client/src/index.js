34f21535c6d603c76faddbb33322cce43d65d774
ankpal145 <97392986+ankpal145@users.noreply.github.com>
2023-06-28 17:08:46 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,22 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/auth";
+import { SearchProvider } from "./context/search";
+import "antd/dist/reset.css";
+import { CartProvider } from "./context/cart";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AuthProvider>
+    <SearchProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
+    </SearchProvider>
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
