6601c04c8000682c334686b6c9d77d6b5dc8d009
ankpal145 <97392986+ankpal145@users.noreply.github.com>
2023-06-28 17:00:50 +0530

    Update Layout

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,25 +1,21 @@
 import React from "react";
-
 import Header from "./Header";
 import Footer from "./Footer";
 import { Helmet } from "react-helmet";
-
-import  { Toaster } from 'react-hot-toast';
-
+import { Toaster } from "react-hot-toast";
 
 const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
       <Helmet>
-        <meta charSet="utf-8" />
+        <meta Charset="utf-8" />
         <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
+        <meta name="Keywords" content={Keywords} />
         <meta name="author" content={author} />
         <title>{title}</title>
       </Helmet>
       <Header />
       <main style={{ minHeight: "70vh" }}>
-        {" "}
         <Toaster />
         {children}
       </main>
@@ -27,6 +23,7 @@ const Layout = ({ children, title, description, keywords, author }) => {
     </div>
   );
 };
+
 Layout.defaultProps = {
   title: "Ecommerce app - shop now",
   description: "mern stack project",
